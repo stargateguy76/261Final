5dab311f51947006560f2a6762afb3aa4ee052db
Allan Huntington <allanhuntington@stargateguy76s-Laptop-1851.local>
2019-08-23 08:36:29 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Set1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Set1.dir/main.cpp.o"
+  "Set1.pdb"
+  "Set1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Set1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
