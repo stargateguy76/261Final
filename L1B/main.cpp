463e9268ecb449ad4e9f43a3bcdda984352422a8
allanhuntington <ahallanhunt7@gmail.com>
2019-08-26 12:46:13 -0600

    updated files

---
+++ L1B/main.cpp
@@ -0,0 +1,35 @@
+/* CSCI 261 Lab 1B: Intro to ACSI art
+ *
+ * Author: Allan Huntington
+ *
+ * More complete description here...
+ */
+
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants or global variables below this comment.
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+
+
+
+    cout << "   *\n"
+            "  ***\n"
+            " *****\n"
+            "*******\n"
+            "  ***\n \n" << endl;
+    cout << "/\\   /\\\n"
+            "  o o\n"
+            " =   =\n"
+            "  ---" << endl;
+     // print Hello world! to the screen
+
+
+
+    return 0; // signals the operating system that our program ended OK.
+}
