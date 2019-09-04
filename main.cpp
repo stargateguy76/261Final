3b4920fb03b7e8e8e5a989bd60408ff594ba6329
allanhuntington <ahallanhunt7@gmail.com>
2019-09-04 12:49:40 -0600

    final A

--- main.cpp
+++ main.cpp
@@ -10,62 +10,60 @@
 #include <iostream>
 #include <cmath>// For cin, cout, etc.
 #include <stdlib.h>
+#include <cstdlib>
 #include <time.h>
-
+#include <ctime>
 // We will (most of the time) use the standard library namespace in our programs.
 using namespace std;
 
 int main() {
-char input;
-int win;
-string compChoice[3]={"Rock","paper","Sciscors"};
-string userChoice[3]={"Rock","paper","Sciscors"};
+    double side1=0;
+    double side2=0;
+    double side3=0;
+    double valTemp=0;
+    double tol=0.001;
+    int sum;
+    double area=0;
+    double perimeter;
+    double S;
+    srand(time(0));
+    rand();
+    string Kind="N/a";
+    cout << "Please enter your two sides" <<endl;
+    cin >>side1 >> side2;
+
+    sum=side1+side2;
+
+    if (side1>side2)
+    {
+        valTemp=side1;
+        side1=side2;
+        side2=valTemp;
+
+    }
+
+    side3=rand() %(sum-int(side2)+1)+int(side2);
+    perimeter=side1+side2+side3;
+    S=perimeter/2;
+    area=sqrt(S*((S-side1)*(S-side2)*(S-side3)));
 
 
-int computerNumber;
-srand(time(0));
 
-cout << "Welcome one and all to a round of Rock, Paper, Scissors! (Enter P, R or S)" << endl;
-cin >> input;
-input=toupper(input);
 
-if (input == 'R')
+    if (fabs(pow(side1,2) + pow(side2,2) - pow(side3,2)) <=tol)
+    {
+        Kind="Right";
+    }
+else if(pow(side1,2)+pow(side2,2)>pow(side3,2))
 {
-    cout <<"Player choose: Rock" <<endl;
-    win =0;
-
+    Kind="Acute";
 }
-if (input == 'S')
-{
-        cout <<"Player choose: Scissors" <<endl;
-        win=2;
-
+else{
+    Kind = "Obtuse";
 }
-if (input == 'P')
-{
-        cout <<"Player choose: Paper" <<endl;
-        win=1;
-
-}
-
-    computerNumber=rand()%3;
-    cout <<"Computer choose: " << compChoice[computerNumber]  << endl;
-
-if (computerNumber==win)
-{
-    cout <<"its a tie!" <<endl;
-}
-if ((computerNumber==0 && win==1)  || (computerNumber==1 && win==2) ||(computerNumber==2 && win==0) )
-{
-        cout <<"user wins" <<endl;
-}
-if ((computerNumber==1 && win== 0)  || (computerNumber==2 && win==1) ||(computerNumber==1 && win==2) )
-{
-        cout <<"computer wins" <<endl;
-}
-
-
-
-
+cout <<"you have a triangle with sides:"<< side1 <<" " << side2<<" " <<side3 <<endl;
+cout << "You have a " << Kind << " triangle"<<endl;
+cout << "the area of the triangle is: " << area <<endl;
+cout<< "the perimeter of the triangle is : " <<perimeter <<endl;
     return 0; // signals the operating system that our program ended OK.
 }
